8a75cb982886d947748d4cf303f7b1eab7ce22ec
Xuhui-12138 <1826870174@qq.com>
2026-07-18 23:44:41 +0800

    11

--- Main.gd
+++ Main.gd
@@ -4,7 +4,7 @@ var score=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,22 +15,39 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$Hud.show_game_over()
+	$Bgm.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score=0
 	$Player.start($StartPosition.position)
-	$ScoreTimer.start()
-
+	$StartTimer.start()
+	$Hud.update_score(score)
+	$Hud.show_message("做好准备！")
+	get_tree().call_group("mobs","queue_free")
+	$Bgm.play()
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
-	
-	var mob_spawn_location=$MobPath/MobSpawnLocation
-	mob_spawn_location.progress
 
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	var direction = mob_spawn_location.rotation + PI / 2
+	mob.position = mob_spawn_location.position
+
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	add_child(mob)  # 必须加进场景
 
 func _on_score_timer_timeout() -> void:
 	score +=1
+	$Hud.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
---
+++ hud.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+signal start_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func show_message(text):
+	$Message.text=text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("游戏结束")
+	await $MessageTimer.timeout
+	
+	$Message.text="躲避怪物
+积累分数"
+	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text=str(score)
+	
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout() -> void:
+	$Message.hide()
